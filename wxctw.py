df2964706b6ac235704c7976c164039903e72a09
msoulier <msoulier@c36970fe-93d6-0310-93e0-db849999abed>
2004-06-13 23:50:54 +0000

    Rejustified, and added the call to weatherfeed.
    git-svn-id: svn+ssh://72.9.145.229/var/svn/open/trunk/ctw@272 c36970fe-93d6-0310-93e0-db849999abed

--- wxctw.py
+++ wxctw.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# vim: ft=python ts=2 sw=2 et:
 
 "This is a wxPython GUI wrapper for Curse the Weather application."
 
@@ -11,87 +12,88 @@ ID_LOCATION = 103
 ID_UPDATE   = 104
 
 about = """\
-Copyright Dan Cardamore and
+    Copyright Dan Cardamore and
 Michael Soulier, 2004"""
 
 class CtwFrame(wxFrame):
-    def __init__(self, parent, ID, title):
-        wxFrame.__init__(self, parent, ID, title,
-                         wxDefaultPosition, wxSize(500, 500))
-        self.CreateStatusBar()
-        self.SetStatusText("This is the statusbar")
+  def __init__(self, parent, ID, title):
+    wxFrame.__init__(self, parent, ID, title,
+        wxDefaultPosition, wxSize(500, 500))
+    self.CreateStatusBar()
+    self.SetStatusText("This is the statusbar")
 
-        menuFile = wxMenu()
-        menuFile.Append(ID_ABOUT, "&About",
-                "More information about this program")
-        menuFile.AppendSeparator()
-        menuFile.Append(ID_EXIT, "E&xit", "Terminate the program")
+    menuFile = wxMenu()
+    menuFile.Append(ID_ABOUT, "&About",
+        "More information about this program")
+    menuFile.AppendSeparator()
+    menuFile.Append(ID_EXIT, "E&xit", "Terminate the program")
 
-        menuSettings = wxMenu()
-        menuSettings.Append(ID_LOCATION, "&Location",
-                "Set your desired location")
-        menuSettings.Append(ID_UPDATE, "&Update",
-                "Update weather data by current location")
+    menuSettings = wxMenu()
+    menuSettings.Append(ID_LOCATION, "&Location",
+        "Set your desired location")
+    menuSettings.Append(ID_UPDATE, "&Update",
+        "Update weather data by current location")
 
-        menuBar = wxMenuBar()
-        menuBar.Append(menuFile, "&File");
-        menuBar.Append(menuSettings, "&Settings");
+    menuBar = wxMenuBar()
+    menuBar.Append(menuFile, "&File");
+    menuBar.Append(menuSettings, "&Settings");
 
-        self.SetMenuBar(menuBar)
+    self.SetMenuBar(menuBar)
 
-        self.location = ""
+    self.location = "CAXX0343"
+    self.weather = None
 
-        EVT_MENU(self, ID_ABOUT, self.OnAbout)
-        EVT_MENU(self, ID_EXIT, self.OnExit)
-        EVT_MENU(self, ID_LOCATION, self.OnLocation)
-        EVT_MENU(self, ID_UPDATE, self.OnUpdate)
+    EVT_MENU(self, ID_ABOUT, self.OnAbout)
+    EVT_MENU(self, ID_EXIT, self.OnExit)
+    EVT_MENU(self, ID_LOCATION, self.OnLocation)
+    EVT_MENU(self, ID_UPDATE, self.OnUpdate)
 
-    def OnUpdate(self, event):
-        max = 100
-        dialog = wxProgressDialog(
-                "Updating weather data",
-                "Please wait",
-                maximum = max,
-                parent = self,
-                style = wxPD_CAN_ABORT | wxPD_APP_MODAL)
-        keep_going = True
-        progress = 0
-        while keep_going and progress < max:
-            progress += 5
-            wxSleep(1)
-            keep_going = dialog.Update(progress, "%d%%" % progress)
-        dialog.Destroy()
+  def OnUpdate(self, event):
+    self.weather = weatherfeed.Weather(self.location)
+#        max = 100
+#        dialog = wxProgressDialog(
+#                "Updating weather data",
+#                "Please wait",
+#                maximum = max,
+#                parent = self,
+#                style = wxPD_CAN_ABORT | wxPD_APP_MODAL)
+#        keep_going = True
+#        progress = 0
+#        while keep_going and progress < max:
+#            progress += 5
+#            keep_going = dialog.Update(progress, "%d%%" % progress)
+#        dialog.Destroy()
 
-    def OnLocation(self, event):
-        dialog = wxTextEntryDialog(
-                self,
-                "Enter location code",
-                "Location",
-                self.location)
-        if dialog.ShowModal() == wxID_OK:
-            self.location = dialog.GetValue()
-            print "location is %s" % self.location
-        dialog.Destroy()
+  def OnLocation(self, event):
+    dialog = wxTextEntryDialog(
+      self,
+      "Enter location code",
+      "Location",
+      self.location)
+    if dialog.ShowModal() == wxID_OK:
+      self.location = dialog.GetValue()
+      print "location is %s" % self.location
+    dialog.Destroy()
 
-    def OnAbout(self, event):
-        global dialog
-        dialog = wxMessageDialog(self, about, "Curse the Weather", wxOK)
-        dialog.ShowModal()
-        dialog.Destroy()
+  def OnAbout(self, event):
+    global dialog
+    dialog = wxMessageDialog(self, about, "Curse the Weather", wxOK)
+    dialog.ShowModal()
+    dialog.Destroy()
 
-    def OnExit(self, event):
-        self.Close(true)
+  def OnExit(self, event):
+    self.Close(true)
 
 class CtwGui(wxApp):
-    def OnInit(self):
-        frame = CtwFrame(NULL, -1, "Curse the Weather")
-        frame.Show(true)
-        self.SetTopWindow(frame)
-        return true
+  def OnInit(self):
+    frame = CtwFrame(NULL, -1, "Curse the Weather")
+    frame.Show(true)
+    self.SetTopWindow(frame)
+    return true
 
 def main():
-    gui = CtwGui(0)
-    gui.MainLoop()
+  gui = CtwGui(0)
+  gui.MainLoop()
 
 if __name__ == '__main__':
-    main()
+  main()
