cde0934f0801f4bcddf8a85fbfe9b5eedd1647ee
dan <dan@c36970fe-93d6-0310-93e0-db849999abed>
2008-10-12 23:59:57 +0000

    Version 0.4
    Using default ncurses colours which allows picking up transparency.
    
    
    
    git-svn-id: svn+ssh://72.9.145.229/var/svn/open/trunk/ctw@314 c36970fe-93d6-0310-93e0-db849999abed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name="CurseTheWeather",
-		version="0.3",
+		version="0.4",
 		description="Weather feed backend and ncurses front end for current and forecast weather",
 		author="Dan Cardamore",
 		author_email="dan@hld.ca",
